e8a55cb1e4d8f274ad7b331b265eaadba4c671ae
j- <j@skeoh.com>
2020-08-09 19:08:26 +1000

    Updates

---
+++ precache-manifest.48c0bb98b26992a99d2c1d4810d648c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f71b5597e74617c09b237e820c73153",
+    "url": "/canvas-arc/index.html"
+  },
+  {
+    "revision": "b30d4f1c3dfcbb81ea50",
+    "url": "/canvas-arc/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "67431314ce03c919c8c4",
+    "url": "/canvas-arc/static/css/main.b60c93d4.chunk.css"
+  },
+  {
+    "revision": "b30d4f1c3dfcbb81ea50",
+    "url": "/canvas-arc/static/js/2.8b6e992c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/canvas-arc/static/js/2.8b6e992c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67431314ce03c919c8c4",
+    "url": "/canvas-arc/static/js/main.b96a20ea.chunk.js"
+  },
+  {
+    "revision": "acece4c0b54143ea776a",
+    "url": "/canvas-arc/static/js/runtime-main.da787a55.js"
+  }
+]);
